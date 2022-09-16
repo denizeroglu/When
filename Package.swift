b355a7f06ff746631c6c901676dfaf0802023d61
Deniz Eroglu <denizeroglu@me.com>
2022-09-16 19:54:31 +0200

    Update quick and nimble dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Quick.git",
-                 .upToNextMinor(from: "2.2.0")),
+                 .upToNextMinor(from: "5.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git",
-                 .upToNextMinor(from: "8.0.5"))
+                 .upToNextMinor(from: "10.0.0"))
     ],
     targets: [
         .target(name: "When",
